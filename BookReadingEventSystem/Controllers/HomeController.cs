e1a0893d0c6e99b46db658edc0b5fb0ce6606f1f
tyreyn-laptop <athosikish33@gmail.com>
2024-06-09 21:03:42 +0200

    free time

--- BookReadingEventSystem/Controllers/HomeController.cs
+++ BookReadingEventSystem/Controllers/HomeController.cs
@@ -1,15 +1,26 @@
 using BookReadingEventSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
 using System.Diagnostics;
+using BookReadingEventSystemDTO.Operations;
+using BookReadingEventSystemDTO.Data;
 namespace BookReadingEventSystem.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private UserOperations _userOperations;
+
+        public HomeController(ILogger<HomeController> logger, BookReadingEventSystemContext bookReadingEventSystemContext)
         {
             _logger = logger;
+            _userOperations = new UserOperations(bookReadingEventSystemContext);
+        }
+
+        public bool CheckLoginUser()
+        {
+            return true;
         }
 
         public IActionResult Index()
---
+++ BookReadingEventSystemDTO/Operations/UserOperations.cs
@@ -0,0 +1,21 @@
+﻿using BookReadingEventSystemDTO.Data;
+using BookReadingEventSystemDTO.Models;
+
+namespace BookReadingEventSystemDTO.Operations
+{
+    public class UserOperations
+    {
+        private BookReadingEventSystemContext _context;
+
+        public UserOperations(BookReadingEventSystemContext bookReadingEventSystemContext)
+        {
+            _context = bookReadingEventSystemContext;
+        }
+
+        public bool AddUser(User user)
+        {
+            return true;
+        }
+
+    }
+}
